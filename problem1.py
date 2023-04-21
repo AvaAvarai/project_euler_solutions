12c381e6ea59be402e514dc1d97e037a7745f619
Alice Williams <alice.williams.tech@gmail.com>
2023-04-21 10:31:30 -0700

    p1 comments

--- problem1.py
+++ problem1.py
@@ -1,3 +1,12 @@
+"""
+Problem #1:
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
+The sum of these multiples is 23.
+Find the sum of all the multiples of 3 or 5 below 1000.
+"""
+
+# This solution makes use of the Sieve of Eratosthenes.
+
 import math
 
 # first solution
@@ -16,4 +25,5 @@ import math
 # print(math.fsum(nums))
 
 # second solution
+# This solution uses the same algorithm in a single loop.
 print(math.fsum(i for i in range(2, 1000) if i % 3 == 0 or i % 5 == 0))

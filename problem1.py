5a317e24a735fa498b6fa79ef735d317d822f029
Alice Williams <alice.williams.tech@gmail.com>
2023-04-21 10:22:59 -0700

    refactor to one-liner

--- problem1.py
+++ problem1.py
@@ -1,15 +1,19 @@
 import math
 
-lst = [i for i in range(2, 1000)]
+# first solution
+# lst = [i for i in range(2, 1000)]
+#
+# nums = []
+#
+# for i in lst:
+#     if i % 3 == 0:
+#         nums.append(i)
+#         continue
+#     if i % 5 == 0:
+#         nums.append(i)
+#         continue
+#
+# print(math.fsum(nums))
 
-nums = []
-
-for i in lst:
-    if i % 3 == 0:
-        nums.append(i)
-        continue
-    if i % 5 == 0:
-        nums.append(i)
-        continue
-
-print(math.fsum(nums))
+# second solution
+print(math.fsum(i for i in range(2, 1000) if i % 3 == 0 or i % 5 == 0))
